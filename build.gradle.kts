b27108578a3ef38a898d5184c91a4dba0c3b7b59
Paul Mooney <paul.mooney@live.com>
2022-04-30 13:56:25 -0400

    Upgrade JVM to Java 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,15 +3,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.5.5"
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
-	kotlin("jvm") version "1.5.31"
-	kotlin("plugin.spring") version "1.5.31"
+	kotlin("jvm") version "1.6.21"
+	kotlin("plugin.spring") version "1.6.21"
 }
 
 val kotestVersion = "4.4.3"
 
 group = "com.contended"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
 	mavenCentral()
